7299adba9c14dcb2783d7bc73512e8e42c827028
Rafael Luiz Casa <rafael.casa@pm.me>
2021-11-16 23:11:07 -0300

    Update main.ino
    v1

--- main/main.ino
+++ main/main.ino
@@ -12,15 +12,21 @@
 //Configuraçao de tempo entre acionamentos de botões para o debounce (em ms)
 #define TEMPO_DEBOUNCE 500 
 
-
 // Configuração dos valores mínimo e máximo do sensor de temperatura para a conversão de volts para graus celsius
 #define VALOR_MINIMO_SENSOR_TEMPERATURA 2
 #define VALOR_MAXIMO_SENSOR_TEMPERATURA 150
+
+#define FREQUENCIA_PWM 1000
+#define CANAL_PWM 0
 
 // Variáveis Globais
 unsigned long timestamp_ultimo_acionamento_liga_desliga = 0;
 unsigned long timestamp_ultimo_acionamento_reset_alerta = 0;
 
+unsigned int setpoint = 65;
+unsigned int limite_alerta = 115;
+unsigned int temperatura_lida;
+unsigned int pwm_motor;
 
 bool ligado = false;
 bool alerta = false;
@@ -42,7 +48,8 @@ void IRAM_ATTR reset_alerta() {
   // Debounce por software
   if ( (millis() - timestamp_ultimo_acionamento_reset_alerta) >= TEMPO_DEBOUNCE ) {
     if(alerta) {
-    alerta = false;
+     alerta = false;
+     digitalWrite(LED_ALERTA, LOW);
     }
     timestamp_ultimo_acionamento_reset_alerta = millis();
   }
@@ -54,8 +61,10 @@ void setup() {
   pinMode(BOTAO_LIGA_DESLIGA, INPUT);
   pinMode(BOTAO_RESET_ALERTA, INPUT);
   pinMode(SENSOR_TEMPERATURA, INPUT);
-  pinMode(MOTOR_PWM, OUTPUT);
   pinMode(LED_ALERTA, OUTPUT);
+
+  ledcSetup(CANAL_PWM, FREQUENCIA_PWM, 8);
+  ledcAttachPin(MOTOR_PWM, CANAL_PWM);
 
   attachInterrupt(BOTAO_LIGA_DESLIGA, liga_desliga, FALLING);
   attachInterrupt(BOTAO_RESET_ALERTA, reset_alerta, FALLING);
@@ -63,25 +72,45 @@ void setup() {
   Serial.begin(115200);
 }
 
-// the loop function runs over and over again forever
+
 void loop() {
   unsigned int leitura_adc;
-  int temperatura;
+  unsigned long tempo1, tempo2;
 
+  tempo1 = millis();
   leitura_adc = analogRead(SENSOR_TEMPERATURA);
-  temperatura = map(leitura_adc, 0, 4096, VALOR_MINIMO_SENSOR_TEMPERATURA, VALOR_MAXIMO_SENSOR_TEMPERATURA);
+  temperatura_lida = map(leitura_adc, 0, 4096, VALOR_MINIMO_SENSOR_TEMPERATURA, VALOR_MAXIMO_SENSOR_TEMPERATURA);
 
-  Serial.println(temperatura);
-  
   if(ligado) {
-    digitalWrite(MOTOR_PWM, HIGH);
+    if(temperatura_lida > limite_alerta) {
+      alerta = true;
+      ligado = false;
+      pwm_motor = 0;
+      digitalWrite(LED_ALERTA, HIGH);
+    } else {
+      if(temperatura_lida < setpoint) {
+        pwm_motor = 255;
+      } else {
+        pwm_motor = map(temperatura_lida, setpoint, limite_alerta, 0, 255);
+      }
+    }
   } else {
-    digitalWrite(MOTOR_PWM, LOW);
+    pwm_motor = 0;
   }
-  if(alerta) {
-    digitalWrite(LED_ALERTA, HIGH);
-  } else {
-    digitalWrite(LED_ALERTA, LOW);
-  }
+
+  ledcWrite(CANAL_PWM, pwm_motor);
+  tempo2 = millis();
+  
+  Serial.print("SETPOINT: ");
+  Serial.println(setpoint);
+  Serial.print("LIMITE ALERTA: ");
+  Serial.println(limite_alerta);
+  Serial.print("TEMPERATURA: ");
+  Serial.println(temperatura_lida);
+  Serial.print("PWM MOTOR: ");
+  Serial.println(pwm_motor);
+  Serial.print("TEMPO EXECUCAO: ");
+  Serial.println(tempo2 - tempo1);
+  
   delay(500);        
 }
