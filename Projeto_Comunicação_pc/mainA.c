15de93da168ef07c3cae93da33fc7c658195c0e6
Rafael Luiz Casa <rafael.casa@pm.me>
2021-11-19 15:32:42 -0300

    ident

--- Projeto_Comunicação_pc/mainA.c
+++ Projeto_Comunicação_pc/mainA.c
@@ -22,190 +22,189 @@
 #define STOPBITS ONESTOPBIT     // stop bits (ONESTOPBIT, TWOSTOPBITS, ONE5STOPBITS)
 
 void flush_in() {
-    int ch;
-    do {
-        ch = fgetc(stdin);
-    } while (ch != EOF && ch != '\n');
+	int ch;
+	do {
+		ch = fgetc(stdin);
+	} while (ch != EOF && ch != '\n');
 }
 
 // metodo principal
 int main(int argc, char *argv[])
 {
 
-  char c;
-  unsigned short setpoint=50;
-  unsigned short limite=80;
-  unsigned short aux;
-  unsigned short saida;
-  unsigned short temp;
-  unsigned short i;
-  unsigned short crc16_result;
-  unsigned char buffer[10];
-  unsigned char recebido[10];
-  // cria uma estrutura para manipular uma porta serial
-  SerialPort s;
-  // SerialPort s1;     // cria uma outra porta serial
+	char c;
+	unsigned short setpoint=50;
+	unsigned short limite=80;
+	unsigned short aux;
+	unsigned short saida;
+	unsigned short temp;
+	unsigned short i;
+	unsigned short crc16_result;
+	unsigned char buffer[10];
+	unsigned char recebido[10];
+	// cria uma estrutura para manipular uma porta serial
+	SerialPort s;
+	// SerialPort s1;     // cria uma outra porta serial
 
-   //abre a porta de comunicacao com as definicoes
-  if(!serialOpen(&s, PORTA_COM,BAUDRATE,8,PARIDADE,STOPBITS))
-  {
-      printf("Erro de inicializacao.\n");
-      system("PAUSE"); return -1;
-  }
+	//abre a porta de comunicacao com as definicoes
+	if(!serialOpen(&s, PORTA_COM,BAUDRATE,8,PARIDADE,STOPBITS))
+	{
+		printf("Erro de inicializacao.\n");
+		system("PAUSE"); return -1;
+	}
 
-  while(c != 'x'){
-  	
-  	system("cls");
-  
+	while(c != 'x'){
 
-    printf("Digite a opcao desejada (x para encerrar):\n");
-    printf("1. Definir setpoint\n");
-    printf("2. Definir limite\n");
-    printf("3. Monitorar variaveis\n");
-    printf("Opcao: ");
+		system("cls");
 
-    c = getchar();
-    flush_in();
 
-    switch (c){
-    case '1':
-    	while(1){
-			system("cls");
-			printf("Limite: %d",limite);
-	        printf("\nSetpoint: %d",setpoint);
-	        printf("\n\n0. Retornar\n\n");
-	        printf("Novo setpoint: ");
-	        scanf("%d", &aux);
-	        flush_in();
-	        
-	        if(aux==0) {
-	        	system ("cls"); 
-				break;
-			}
-	        else if(aux>0 && aux<=80) {
-				setpoint=aux;
-				
-				buffer[0] = 0x0A;
-				buffer[1] = 0xC4;
-				buffer[2] = setpoint;
-				
-				crc16_result = crc16_ccitt(buffer,3);
-				
-				serialPutBytes(&s, buffer, 3);
-				serialPutc(&s, crc16_result>>8);
-				serialPutc(&s, crc16_result & 0xFF);
-			}
-				
-	        else {
-	        	printf("\nSetpoint invalido\n");
-	        	system("PAUSE");
-			}
-        
-    	}
-        break;
-    case '2':
-        while(1){
-	        system("cls");
-			
-	        printf("Limite: %d",limite);
-	        printf("\nSetpoint: %d",setpoint);
-	        printf("\n\n0. Retornar\n\n");
-	        printf("Novo limite: ");
-	        scanf("%d", &aux);
-	        flush_in();
-	        
-	        if(aux==0) {
-	        	system ("cls"); 
-				break;
-			}
-	        else if(aux>setpoint && aux<=150) {
-				limite=aux;
-				
-				buffer[0] = 0x0A;
-				buffer[1] = 0xF1;
-				buffer[2] = limite;
-				
-				crc16_result = crc16_ccitt(buffer,3);
-				
-				serialPutBytes(&s, buffer, 3);
-				serialPutc(&s, crc16_result>>8);
-				serialPutc(&s, crc16_result & 0xFF);
-				
-			}
-	        else {
-	        	printf("\nLimite invalido\n");
-	        	system("PAUSE");
-			}
-	        
-	    	}
-        break;
-    case '3':
-    	while(1){
-    		
-    		if(kbhit()) {
-    			flush_in();
-    			break;	
-    		}
-				
+		printf("Digite a opcao desejada (x para encerrar):\n");
+		printf("1. Definir setpoint\n");
+		printf("2. Definir limite\n");
+		printf("3. Monitorar variaveis\n");
+		printf("Opcao: ");
 
-				buffer[0] = 0x0A;
-				buffer[1] = 0x00;
-				buffer[2] = 0x00;
-				buffer[3] = 0xC7;
-				buffer[4] = 0xC1;
-    		serialPutBytes(&s, buffer, 5);
+		c = getchar();
+		flush_in();
 
-    		if(serialGetc(&s) == 0x3F){
-				recebido[0] = 0x3F;
-				i=1;
-	    		while(serialBytesReceived(&s)){
-	    			recebido[i] = serialGetc(&s);
-	    			i++;
+		switch (c){
+			case '1':
+				while(1){
+					system("cls");
+					printf("Limite: %d",limite);
+					printf("\nSetpoint: %d",setpoint);
+					printf("\n\n0. Retornar\n\n");
+					printf("Novo setpoint: ");
+					scanf("%d", &aux);
+					flush_in();
+
+					if(aux==0) {
+						system ("cls"); 
+						break;
+					}
+					else if(aux>0 && aux<=80) {
+						setpoint=aux;
+
+						buffer[0] = 0x0A;
+						buffer[1] = 0xC4;
+						buffer[2] = setpoint;
+
+						crc16_result = crc16_ccitt(buffer,3);
+
+						serialPutBytes(&s, buffer, 3);
+						serialPutc(&s, crc16_result>>8);
+						serialPutc(&s, crc16_result & 0xFF);
+					}
+
+					else {
+						printf("\nSetpoint invalido\n");
+						system("PAUSE");
+					}
+
 				}
-			}
-			
-			crc16_result = crc16_ccitt(recebido,5);
-			
-			if(crc16_result == (recebido[5]<<8) + recebido[6]){
-			
-    	
-			setpoint = recebido[1];
-			limite = recebido[2];	
-			temp = recebido[3];
-			saida = recebido[4] * 100 / 255;
-			
-			}
-			
-			system("cls");
-			
-	        printf("Variaveis: ");
-	        printf("\nLimite: %d",limite);
-	        printf("\nSetpoint: %d",setpoint);
-	        printf("\nTemperatura: %d",temp);
-	        printf("\nMotor: %d", saida);
-	        printf("\nQualquer tecla para Retornar\n");
-	        Sleep(500);
-	        }
-        break;
-    case 'x':
-    	break;
-    default:
-    	printf("Erro, tente de novo\n");
-    	system("PAUSE");
-    
-    }
-    
-}
+				break;
+			case '2':
+				while(1){
+					system("cls");
+
+					printf("Limite: %d",limite);
+					printf("\nSetpoint: %d",setpoint);
+					printf("\n\n0. Retornar\n\n");
+					printf("Novo limite: ");
+					scanf("%d", &aux);
+					flush_in();
+
+					if(aux==0) {
+						system ("cls"); 
+						break;
+					}
+					else if(aux>setpoint && aux<=150) {
+						limite=aux;
+
+						buffer[0] = 0x0A;
+						buffer[1] = 0xF1;
+						buffer[2] = limite;
+
+						crc16_result = crc16_ccitt(buffer,3);
+
+						serialPutBytes(&s, buffer, 3);
+						serialPutc(&s, crc16_result>>8);
+						serialPutc(&s, crc16_result & 0xFF);
+
+					}
+					else {
+						printf("\nLimite invalido\n");
+						system("PAUSE");
+					}
+
+				}
+				break;
+			case '3':
+				while(1){
+
+					if(kbhit()) {
+						flush_in();
+						break;	
+					}
 
 
-   //finaliza a porta de comunicacao
-  if(!serialClose(&s))
-  {
-      printf("Erro de finalizacao.\n");
-      system("PAUSE"); return -1;
-  }
+					buffer[0] = 0x0A;
+					buffer[1] = 0x00;
+					buffer[2] = 0x00;
+					buffer[3] = 0xC7;
+					buffer[4] = 0xC1;
+					serialPutBytes(&s, buffer, 5);
 
-  // encerra o programa
-  system("PAUSE");
-  return 0;
+					if(serialGetc(&s) == 0x3F){
+						recebido[0] = 0x3F;
+						i=1;
+						while(serialBytesReceived(&s)){
+							recebido[i] = serialGetc(&s);
+							i++;
+						}
+					}
+
+					crc16_result = crc16_ccitt(recebido,5);
+
+					if(crc16_result == (recebido[5]<<8) + recebido[6]){
+
+						setpoint = recebido[1];
+						limite = recebido[2];	
+						temp = recebido[3];
+						saida = recebido[4] * 100 / 255;
+
+					}
+
+					system("cls");
+
+					printf("Variaveis: ");
+					printf("\nLimite: %d",limite);
+					printf("\nSetpoint: %d",setpoint);
+					printf("\nTemperatura: %d",temp);
+					printf("\nMotor: %d", saida);
+					printf("\nQualquer tecla para Retornar\n");
+					Sleep(500);
+				}
+				break;
+			case 'x':
+				break;
+			default:
+				printf("Erro, tente de novo\n");
+				system("PAUSE");
+
+		}
+
+	}
+
+
+	//finaliza a porta de comunicacao
+	if(!serialClose(&s))
+	{
+		printf("Erro de finalizacao.\n");
+		system("PAUSE"); return -1;
+	}
+
+	// encerra o programa
+	system("PAUSE");
+	return 0;
 }
