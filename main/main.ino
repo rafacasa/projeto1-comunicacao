e8fdb1c894833a85b33bdbecdcfd2620df7b1856
Rafael Luiz Casa <rafael.casa@pm.me>
2021-11-19 00:08:55 -0300

    comentarios

--- main/main.ino
+++ main/main.ino
@@ -2,46 +2,61 @@
   Trabalho 1 de Comunicação de Dados
 */
 
-#include <Crc16.h>    // biblioteca de funcoes CRC-16
+// Inclusão de bibliotecas
+// Cálculo do CRC
+#include <Crc16.h>
 
-// Pinagem das funções
+
+// Constantes
+// Pinos utilizados no sistema
 const uint8_t BOTAO_LIGA_DESLIGA = 32;
 const uint8_t BOTAO_RESET_ALERTA = 22;
 const uint8_t MOTOR_PWM = 12;
 const uint8_t LED_ALERTA = 13;
 const uint8_t SENSOR_TEMPERATURA = 36;
 
-//Configuraçao de tempo entre acionamentos de botões para o debounce (em ms)
+// Configuraçao de tempo entre acionamentos de botões para o debounce (em ms)
 const uint16_t TEMPO_DEBOUNCE = 500;
 
 // Configuração dos valores mínimo e máximo do sensor de temperatura para a conversão de volts para graus celsius
 const uint8_t VALOR_MINIMO_SENSOR_TEMPERATURA = 2;
 const uint8_t VALOR_MAXIMO_SENSOR_TEMPERATURA = 150;
 
+// Configuração do PWM
 const uint16_t FREQUENCIA_PWM = 1000;
 const uint8_t CANAL_PWM = 0;
 
+
 // Variáveis Globais
+// Auxiliares para o debounce dos botões
 unsigned long timestamp_ultimo_acionamento_liga_desliga = 0;
 unsigned long timestamp_ultimo_acionamento_reset_alerta = 0;
 
+// Dados do sistema
 uint8_t setpoint = 40;
 uint8_t limite_alerta = 115;
 uint8_t temperatura_lida;
 uint8_t pwm_motor;
 
+// Variáveis que são modificadas na interrupção do timer
 volatile uint16_t leitura_adc;
 volatile bool leitura_temperatura_feita;
 
+// Estados do sistema
 bool ligado = false;
 bool alerta = false;
 
+// Buffer de leitura do serial
 uint8_t buffer_leitura[5]; 
 
+// Ponteiro para o timer
 hw_timer_t * timer = NULL;
 
+// Cálculo do CRC
 Crc16 crc;
 
+
+// Funções
 // Função chamada pela interrupção acionada pelo botão liga desliga
 void IRAM_ATTR liga_desliga() {
   // Debounce por software
@@ -65,12 +80,13 @@ void IRAM_ATTR reset_alerta() {
   }
 }
 
+// Função chamada pela interrupção do timer
 void IRAM_ATTR leitura_temperatura_timer() {
   leitura_adc = analogRead(SENSOR_TEMPERATURA);
   leitura_temperatura_feita = true;
 }
 
-
+// Função que envia os dados do sistema pela conexão serial
 void enviar_dados() {
   uint8_t buffer_envio[7];
   uint16_t valor_crc;
@@ -96,12 +112,15 @@ void setup() {
   pinMode(SENSOR_TEMPERATURA, INPUT);
   pinMode(LED_ALERTA, OUTPUT);
 
+  // Setup do PWM
   ledcSetup(CANAL_PWM, FREQUENCIA_PWM, 8);
   ledcAttachPin(MOTOR_PWM, CANAL_PWM);
 
+  // Interrupções por pinos
   attachInterrupt(BOTAO_LIGA_DESLIGA, liga_desliga, FALLING);
   attachInterrupt(BOTAO_RESET_ALERTA, reset_alerta, FALLING);
 
+  // Interrupção do timer
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &leitura_temperatura_timer, true);
   timerAlarmWrite(timer, 500000, true);
@@ -112,7 +131,8 @@ void setup() {
 
 
 void loop() {
-  
+
+  // Controle do processo
   if (leitura_temperatura_feita) {
     temperatura_lida = map(leitura_adc, 0, 4096, VALOR_MINIMO_SENSOR_TEMPERATURA, VALOR_MAXIMO_SENSOR_TEMPERATURA);
   
@@ -136,7 +156,7 @@ void loop() {
     ledcWrite(CANAL_PWM, pwm_motor);
   }
 
-
+  // Comunicação serial
   if(Serial.available() >= 5) {
     Serial.readBytes(buffer_leitura, 5);
     if(buffer_leitura[0] == 0x0A) {
